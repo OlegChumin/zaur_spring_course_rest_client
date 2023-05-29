b2617314ff08ca1851ade89a895bfa2922595ea9
OlegChumin <tschumin.oleg@gmail.com>
2023-05-29 18:09:07 +0300

    Add new files

---
+++ src/main/java/com/zaurtregulov/spring/rest/App.java
@@ -0,0 +1,22 @@
+package com.zaurtregulov.spring.rest;
+
+import com.zaurtregulov.spring.rest.configuration.MyConfig;
+import com.zaurtregulov.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+    }
+}
